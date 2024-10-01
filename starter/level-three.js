1d0f04787cc664c1efbe89cf3545cca13e332b4a
XOKEESH <hello@kishamartin.com>
2024-10-01 00:45:57 -0700

    Completed Project

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,26 @@
  * DOTS: Level Three
  *
  */
+
+
+// Small=20 Large=5 Medium=10
+
+let score = 0
+
+const balls = document.querySelectorAll('.js-ball')
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+balls.forEach(ball => {
+ball.addEventListener ('click', () => {
+    const incrementValue = parseInt(ball.getAttribute('data-increment'))
+    console.log(incrementValue)    
+    score += incrementValue 
+    scoreDisplay.innerText = score.toString()
+    if (score >= 100) {
+        levelWinner.style.setProperty ('opacity', 1)
+    }
+})
+})
+
+//??? I'm soooo confused. - It works, but I dont understand how. Worked through this with Mike D.
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,20 @@
  * DOTS: Level Two
  *
  */
+
+
+let score = 0
+
+const balls = document.querySelectorAll('.js-ball')
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+balls.forEach(ball => {
+ball.addEventListener ('click', () => {
+    score += 10; 
+    scoreDisplay.innerText = score.toString();
+    if (score >= 100) {
+        levelWinner.style.setProperty ('opacity', 1);
+    }
+})
+})
